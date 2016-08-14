f48c7f2f3ae927a533ecd3b207c269160b95529c
Lunik <guillaume.lunik@gmail.com>
2016-08-14 22:13:38 +0200

    add command worker

--- test/test.js
+++ test/test.js
@@ -85,10 +85,10 @@ describe('Module', function () {
   modules.forEach(function (module) {
     describe(module.name, function () {
       var m = require(path.join(__base, 'src/modules/' + module.name + '.js'))
-      module.tests.forEach(function (test) {
-        describe('run(' + test.name + ')', function () {
-          this.timeout(10000)
-          it('Sould return string.', function (done) {
+      describe('run()', function () {
+        this.timeout(10000)
+        module.tests.forEach(function (test) {
+          it(test.name, function (done) {
             m.run(test.user, test.args, function (res) {
               assert.typeOf(res, 'string')
               done()
@@ -104,12 +104,12 @@ describe('Worker', function () {
   describe('Log', function () {
     var Log = require(path.join(__base, 'src/worker/log.js'))
     describe('print()', function () {
-      it('Sould write into log.', function (done) {
-        Log.print('test', function(){
-          fs.readFile(path.join(__base, 'logs/log-' + (new Date()).getDate() + '-' + ((new Date()).getMonth() + 1)), function(err, data) {
+      it('Logs are writted into file.', function (done) {
+        Log.print('test', function () {
+          fs.readFile(path.join(__base, 'logs/log-' + (new Date()).getDate() + '-' + ((new Date()).getMonth() + 1)), function (err, data) {
             if (err) throw err
             var text = data.toString()
-            if(!text.match('test')) throw 'Log not found.'
+            if (!text.match('test')) throw 'Log not found.'
             done()
           })
         })
@@ -118,14 +118,14 @@ describe('Worker', function () {
   })
   describe('Save', function () {
     var Save = require(path.join(__base, 'src/worker/save.js'))
-    describe('Contructor()', function(){
-      it('Should save a struct.', function(done){
-        var interval = new Save('test', {}, 10, function(){
+    describe('Contructor()', function () {
+      it('Save a structure.', function (done) {
+        var interval = new Save('test', {}, 10, function () {
           clearInterval(interval)
-          fs.readFile(path.join(__base, 'data/test.json'), function(err, data) {
+          fs.readFile(path.join(__base, 'data/test.json'), function (err, data) {
             if (err) throw err
             var text = data.toString()
-            if(!text.match('{}')) throw 'Data not found.'
+            if (!text.match('{}')) throw 'Data not found.'
             done()
           })
         })
@@ -133,10 +133,10 @@ describe('Worker', function () {
     })
   })
 
-  describe('Config', function(){
+  describe('Config', function () {
     var Config = require(path.join(__base, 'src/worker/config.js'))
-    describe('Load ()', function(){
-      it('Sould load config.', function(done){
+    describe('Load ()', function () {
+      it('Load config.', function (done) {
         var c = new Config()
         c.load(path.join(__base, 'configs/config.json'))
         done()
@@ -144,34 +144,101 @@ describe('Worker', function () {
     })
   })
 
-  describe('Xp', function(){
+  describe('Xp', function () {
     var Xp = require(path.join(__base, 'src/worker/xp.js'))
     var XpWorker = new Xp(false)
-    describe('Pex(42)', function(){
-      it('Sould return a string', function(done){
+    describe('Pex()', function () {
+      it('User: 42', function (done) {
         var res = XpWorker.pex({
           id: '42'
         })
         assert.typeOf(res, 'string')
         done()
       })
-    })
-    describe('Pex(Unknown)', function(){
-      it('Sould return a string', function(done){
+      it('User: Unknown', function (done) {
         var res = XpWorker.pex({
           id: '7'
         })
         assert.typeOf(res, 'string')
         done()
       })
-    })
-    describe('Pex(Bot)', function(){
-      it('Sould return false', function(done){
+      it('User: Bot', function (done) {
         var res = XpWorker.pex({
           bot: true
         })
         assert(!res)
         done()
+      })
+    })
+  })
+
+  describe('Command', function () {
+    var Command = require(path.join(__base, 'src/worker/command.js'))
+    var CommandWorker = new Command(false)
+    describe('GetCommand(). Command: Ping', function () {
+      it('Command: ping google.fr', function (done) {
+        var res = CommandWorker.getCommand('ping google.fr')
+        assert.typeOf(res, 'object')
+        assert.equal(res.trigger, 'ping')
+        assert.lengthOf(res.args, 1)
+        done()
+      })
+      it('ommand: Unknown', function (done) {
+        var res = CommandWorker.getCommand('')
+        assert.typeOf(res, 'object')
+        assert(!res.trigger)
+        assert.lengthOf(res.args, 0)
+        done()
+      })
+    })
+    describe('Exist()', function () {
+      it('Command: ping google.fr', function (done) {
+        assert(CommandWorker.exist({
+          trigger: 'ping',
+          args: ['google.fr']
+        }))
+        done()
+      })
+      it('Command: Unknown', function (done) {
+        assert(!CommandWorker.exist({
+            trigger: '',
+            args: []
+          }))
+        done()
+      })
+    })
+    describe('isValid()', function () {
+      it('Command: Ping google.fr', function (done) {
+        assert(CommandWorker.isValid({
+          trigger: 'ping',
+          args: ['google.fr']
+        }))
+        done()
+      })
+      it('Command: ping http://google.fr', function (done) {
+        assert(!CommandWorker.isValid({
+            trigger: 'ping',
+            args: ['http://google.fr']
+          }))
+        done()
+      })
+      it('Command: imgur', function (done) {
+        assert(!CommandWorker.isValid({
+            trigger: 'imgur',
+            args: []
+          }))
+        done()
+      })
+    })
+    describe('Process()', function(){
+      it('Command: hi', function(done){
+        CommandWorker.process({}, {
+          trigger: 'hi',
+          args: []
+        }, function(res){
+          assert.typeOf(res, 'string')
+          done()
+        })
       })
     })
   })
