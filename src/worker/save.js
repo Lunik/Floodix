d313fcaccca0e737fb821632fe9c8a11046dca89
Lunik <guillaume.lunik@gmail.com>
2016-08-14 21:34:14 +0200

    return interval

--- src/worker/save.js
+++ src/worker/save.js
@@ -6,7 +6,7 @@ var path = require('path')
 var Log = require(path.join(__base, 'src/worker/log.js'))
 
 function Save (name, struct, interval, cb) {
-  setInterval(function () {
+  return setInterval(function () {
     fs.writeFile(path.join(__config.data.path, name + '.json'), JSON.stringify(struct), 'utf8', cb)
   }, interval)
 }
